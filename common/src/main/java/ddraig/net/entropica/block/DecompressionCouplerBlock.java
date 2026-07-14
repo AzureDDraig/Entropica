45db7cd8370d8854ee009375e3d93ef8dd76efad
AzureDDraig <107419858+AzureDDraig@users.noreply.github.com>
2026-07-14 15:31:05 -0500

    Fix Decompression Coupler block hitbox shape

--- common/src/main/java/ddraig/net/entropica/block/DecompressionCouplerBlock.java
+++ common/src/main/java/ddraig/net/entropica/block/DecompressionCouplerBlock.java
@@ -5,6 +5,7 @@ import ddraig.net.entropica.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -15,14 +16,39 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class DecompressionCouplerBlock extends Block implements EntityBlock {
     public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
 
+    private static final VoxelShape CORE = Block.box(5.0D, 5.0D, 5.0D, 11.0D, 11.0D, 11.0D);
+    private static final VoxelShape ARM_UP = Block.box(5.0D, 11.0D, 5.0D, 11.0D, 16.0D, 11.0D);
+    private static final VoxelShape ARM_DOWN = Block.box(5.0D, 0.0D, 5.0D, 11.0D, 5.0D, 11.0D);
+    private static final VoxelShape ARM_NORTH = Block.box(5.0D, 5.0D, 0.0D, 11.0D, 11.0D, 5.0D);
+    private static final VoxelShape ARM_SOUTH = Block.box(5.0D, 5.0D, 11.0D, 11.0D, 11.0D, 16.0D);
+    private static final VoxelShape ARM_EAST = Block.box(11.0D, 5.0D, 5.0D, 16.0D, 11.0D, 11.0D);
+    private static final VoxelShape ARM_WEST = Block.box(0.0D, 5.0D, 5.0D, 5.0D, 11.0D, 11.0D);
+
+    private static final VoxelShape SHAPE_Y = Shapes.or(CORE, ARM_UP, ARM_DOWN);
+    private static final VoxelShape SHAPE_X = Shapes.or(CORE, ARM_EAST, ARM_WEST);
+    private static final VoxelShape SHAPE_Z = Shapes.or(CORE, ARM_NORTH, ARM_SOUTH);
+
     public DecompressionCouplerBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction.Axis axis = state.getValue(AXIS);
+        return switch (axis) {
+            case X -> SHAPE_X;
+            case Z -> SHAPE_Z;
+            default -> SHAPE_Y;
+        };
     }
 
     @Override
