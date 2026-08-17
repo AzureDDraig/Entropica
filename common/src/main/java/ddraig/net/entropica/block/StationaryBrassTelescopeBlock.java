d273799ba72f92e6592204cd4b15991e9752ef30
AzureDDraig <107419858+AzureDDraig@users.noreply.github.com>
2026-08-17 09:50:34 -0500

    Fix worldgen geode inner_placements BlockState structure, 3D model texture resource locations, telescope horizontal facing, and 1.21.4 item definitions

--- common/src/main/java/ddraig/net/entropica/block/StationaryBrassTelescopeBlock.java
+++ common/src/main/java/ddraig/net/entropica/block/StationaryBrassTelescopeBlock.java
@@ -1,21 +1,42 @@
 package ddraig.net.entropica.block;
 
+import com.mojang.serialization.MapCodec;
 import ddraig.net.entropica.client.gui.ConstellationTracingScreen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
 
-public class StationaryBrassTelescopeBlock extends Block {
+public class StationaryBrassTelescopeBlock extends HorizontalDirectionalBlock {
+    public static final MapCodec<StationaryBrassTelescopeBlock> CODEC = simpleCodec(StationaryBrassTelescopeBlock::new);
 
     public StationaryBrassTelescopeBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
     @Override
